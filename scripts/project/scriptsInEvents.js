5da0481352fbfeee1d6d4791392c842c2ee11950
danascim <74547719+danascim@users.noreply.github.com>
2022-02-17 00:03:40 -0300

    Add files via upload

---
+++ scripts/project/scriptsInEvents.js
@@ -0,0 +1,31 @@
+
+
+
+const scriptsInEvents = {
+
+		async ["Moeda-Moeda_Event4_Act1"](runtime, localVars)
+		{
+			runtime.globalVars.Alt1=0;
+			runtime.globalVars.Alt2=0;
+			runtime.globalVars.Alt3=0;
+			
+			while(runtime.globalVars.Alt1==runtime.globalVars.Alt2 || runtime.globalVars.Alt1==runtime.globalVars.Alt3 || runtime.globalVars.Alt1==0)
+			{
+				runtime.globalVars.Alt1 = Math.floor(Math.random() * 3) + 1;
+			}
+			
+			while(runtime.globalVars.Alt2==runtime.globalVars.Alt1 || runtime.globalVars.Alt2==runtime.globalVars.Alt3 || runtime.globalVars.Alt2==0)
+			{
+				runtime.globalVars.Alt2 = Math.floor(Math.random() * 3) + 1;
+			}
+			
+			while(runtime.globalVars.Alt3==runtime.globalVars.Alt2 || runtime.globalVars.Alt3==runtime.globalVars.Alt1 || runtime.globalVars.Alt3==0)
+			{
+				runtime.globalVars.Alt3 = Math.floor(Math.random() * 3) + 1;
+			}
+		}
+
+};
+
+self.C3.ScriptsInEvents = scriptsInEvents;
+
